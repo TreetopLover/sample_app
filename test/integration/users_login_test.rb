d295c5b7c02f91f9157e79a45113ca498fc17a0d
Joseph Hargrove <joseph.henry.hargrove@gmail.com>
2022-05-25 19:48:05 -0500

    Add account activation

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -40,6 +40,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
 
   test "login with remembering" do
+    @user.reload
     log_in_as(@user, remember_me: '1')
     assert_equal cookies['remember_token'], assigns(:user).remember_token
   end
