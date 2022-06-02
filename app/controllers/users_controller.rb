85da188f2e5981b98c77535c7af143f854ae693e
Joseph Hargrove <joseph.henry.hargrove@gmail.com>
2022-06-02 14:37:22 -0500

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+    # This is the stupid line the tutorial left off that lost me two days of work
     redirect_to root_url and return unless @user.activated?
   end
 
@@ -55,15 +57,6 @@ class UsersController < ApplicationController
     end
 
     # Before filters
-
-    # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
 
     # Confirms the correct user.
     def correct_user
